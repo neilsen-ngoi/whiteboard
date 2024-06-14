5ae4576c50eb4ea32f414ae928e34c595d7997b2
neilsen-ngoi <kenisu222@gmail.com>
2024-06-14 16:51:24 +1200

    add search functions for boards

--- convex/boards.ts
+++ convex/boards.ts
@@ -7,6 +7,8 @@ import { favorite } from "./board";
 export const get = query({
   args: {
     orgId: v.string(),
+    // allow boards to be searchable
+    search: v.optional(v.string()),
   },
   handler: async (ctx, args) => {
     const identity = await ctx.auth.getUserIdentity();
@@ -15,11 +17,24 @@ export const get = query({
       throw new Error("Unauthorized");
     }
 
-    const boards = await ctx.db
-      .query("boards")
-      .withIndex("by_org", (q) => q.eq("orgId", args.orgId))
-      .order("desc")
-      .collect();
+    // allow boards to be searchable
+    const title = args.search as string;
+    let boards = [];
+
+    if (title) {
+      boards = await ctx.db
+        .query("boards")
+        .withSearchIndex("search_title", (q) =>
+          q.search("title", title).eq("orgId", args.orgId)
+        )
+        .collect();
+    } else {
+      boards = await ctx.db
+        .query("boards")
+        .withIndex("by_org", (q) => q.eq("orgId", args.orgId))
+        .order("desc")
+        .collect();
+    }
 
     // create relation with favorites
     const boardsWithFavoriteRelation = boards.map((board) => {
