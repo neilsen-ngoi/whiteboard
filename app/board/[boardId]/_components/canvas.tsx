9aed25f461034d74bf88d26794d24b8738396b95
neilsen-ngoi <kenisu222@gmail.com>
2024-07-05 16:27:49 +1200

    added fill options to selected shape

--- app/board/[boardId]/_components/canvas.tsx
+++ app/board/[boardId]/_components/canvas.tsx
@@ -33,6 +33,7 @@ import { LiveObject } from "@liveblocks/client";
 import { LayerPreview } from "./layer-preview";
 import { SelectionBox } from "./selection-box";
 import { setGlobal } from "next/dist/trace";
+import { SelectionTools } from "./selection-tools";
 
 const MAX_LAYERS = 100;
 
@@ -273,6 +274,7 @@ export const Canvas = ({ boardId }: CanvasProps) => {
         undo={history.undo}
         redo={history.redo}
       />
+      <SelectionTools camera={camera} setLastUsedColor={setLastUsedColor} />
       <svg
         className=" h-[100vh] w-[100vw]"
         onWheel={onWheel}
